7244e58b25b6ac3aa710890a7a4d1d6a29058fbe
Isuru Nanayakkara <16419053+IsuruNana@users.noreply.github.com>
2018-10-03 11:40:27 -0500

    Added some functions

--- haskell/lessons/derek_banas.hs
+++ haskell/lessons/derek_banas.hs
@@ -28,11 +28,34 @@ lastPrime = last primes
 
 -- Returns everything but the last
 primitInit = init primes
+-- 
+
 
 first3Primes = take 3 primes
 
 removedPrimes = drop 3 primes
 
 is7inList = 7 `elem` primes
--- 
 
+-- Takes first 10 of array.Returns [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+cycleList = take 10 (cycle [1, 2, 3, 4, 5])
+
+-- Take x from list 1 to 10 and multiply by 2
+listTimes2 = [x * 2 | x <- [1..10]]
+
+
+-- Take x from list 1 to 10 and multiply by 2. Only pick x based on filter
+listTimes2 = [x * 2 | x <- [1..10], x*3 <= 50]
+
+sumOfLists = zipWith (+) [1, 2, 3, 4, 5] [6, 7, 8, 9, 10]
+
+listBiggerThan5 = filter (>5) primes
+
+evensUpTo20 = takeWhile (<= 20) [2,4..]
+
+
+-- List comprehension
+
+powList = [3^n | n <- [1..10]]
+
+multTable = [[x* y | y<- [1..10]] | x<- [1..10]]
