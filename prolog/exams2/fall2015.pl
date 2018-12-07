8c4a5006082265abe1d9c04922b1b0d277b082f1
Isuru Nanayakkara <isurunanayakkara505@gmail.com>
2018-12-07 11:10:53 -0600

    Reset Prolog exams

---
+++ prolog/exams2/fall2015.pl
@@ -0,0 +1,91 @@
+% 1. Suppose this definition of predicate p is found in Prolog’s database:
+% p(X, X).
+% Determine and explain the output that would appear upon entering this query:
+% ?- p(L, [a|L]).
+% a. First assume that the “occurs check” is implemented in this version of Prolog.
+% b. Next assume that the “occurs check” is not implemented in this version of Prolog.
+% c. Why do most versions of Prolog usually omit performing the “occurs check”?
+
+% 2. Explain why in Prolog the built-in “not” predicate and the cut (“!”) opera
+
+% 3. Suppose a Prolog database defines relations of the form male(X), female(X), sibling(X, Y),
+% spouse(X, Y), and parent(X, Y), which means that X is a parent of Y. Write these Prolog
+% predicates:
+% • nephew(X, Y), which means that X is a nephew of Y.
+% Note: your nephew is your brother’s son, sister’s son, or spouse’s nephew.
+
+
+
+% • niece(X, Y), which means that X is a niece of Y.
+% Note: your niece is your brother’s daughter, sister’s daughter, or spouse’s niece.
+
+
+% • uncle(X, Y), which means that X is an uncle of Y.
+% Note: your uncle is your father’s brother, mother’s brother, or aunt’s husband.
+
+
+% • aunt(X, Y), which means that X is an aunt of Y.
+% Note: your aunt is your father’s sister, mother’s sister, or uncle’s wife.
+
+
+% 4. Suppose a Prolog database contains only facts of the form parent(x, y), which means that X is a
+% parent of Y. Write the Prolog predicate cognate(X, Y), which means that X is blood-related to Y.
+
+
+% 5. Write this predicate using Prolog: function(N, Z) succeeds when Z is the dot product of lists
+% [1,2,…,N–1,N] and [N,N–1,…,2,1]. Examples:
+% function(5, Z) succeeds when Z = 1*5 + 2*4 + 3*3 + 4*2 + 5*1 = 35.
+% function(6, Z) succeeds when Z = 1*6 + 2*5 + 3*4 + 4*3 + 5*2 + 6*1 = 56.
+
+% 6. You are given this Prolog database. Assume that append is defined in the usual way.
+% predicate([ ], [ ]).
+% predicate([H|T], L) :- predicate(H, X), predicate(T, Y), append(X, Y, L).
+% predicate(Z, [Z]).
+% Write all the solutions to Q for this Prolog query:
+% ?- predicate([[ ], 0], Q).
+
+% Q = 
+% Q = 
+% Q = 
+% Q = 
+% Q = 
+% Q =
+
+
+% 7. Write these two predicates using Prolog: power and log. power(M, N, Q) succeeds when Q
+% is the result value obtained by raising M to the power N. log(M, Q, N) succeeds when N is
+% the smallest non-negative integer such that raising M to the power N yields at least Q.
+% Examples: 
+% power(2, 3, Q) yields Q=8. 
+% power(2, 4, Q) yields Q=16.
+% log(2, 8, N) yields N=3. 
+% log(2, 16, N) yields N=4. 
+% log(2, 10, N) yields N=4.
+
+
+% 8. Write a Prolog predicate eval(Expression, Result) that evaluates a prefix Boolean expression
+% over constant values {true, false} and logical operations {not, and, or}. Examples:
+% eval(or(false, and(true, not(false))), X) succeeds with X = true.
+% eval(and(true, or(false, not(true))), Y) succeeds with Y = false.
+
+% 9. Write a Prolog predicate isTrue(Expression, List) that succeeds iff the given prefix Boolean
+% expression evaluates to true in an environment where the variables in the given list are true
+% and all other variables are assumed to be false. Examples:
+% isTrue(or(a, and(b, not(c))), [b]) succeeds.
+% isTrue(or(a, and(b, not(c))), [b,c]) fails.
+% isTrue(or(a, and(b, not(c))), [a,b,c]) succeeds.
+% isTrue(and(x, or(y, not(z))), [x]) succeeds.
+% isTrue(and(x, or(y, not(z))), [x,z]) fails.
+% isTrue(and(x, or(y, not(z))), [x,y,z]) succeeds.
+
+% 10. Explain the differences between imperative programming and declarative programming. Also
+% explain the differences between procedural programming, functional programming, objectoriented
+% programming, and logic programming. Specify the relevant features that are
+% characteristic of each paradigm, that is, the features that must be present in any programming
+% language that fully supports the paradigm.
+
+
+% 11. Write this predicate using Prolog: merge_sort(X, Y) succeeds if Y is the list that is obtained
+% by sorting the elements of list X into ascending order. Your code must use the merge sort
+% algorithm. Do not use any other sorting algorithm such as insertion sort, selection sort, or
+% quick sort. Example: merge_sort([9, 5, 2, 7, 4, 0], L) succeeds with L = [0, 2, 4, 5, 7, 9]. 
---
+++ prolog/exams2/spring2015.pl
@@ -0,0 +1,81 @@
+% 1. Write this predicate using Prolog: inner(X,Y,Z) succeeds when Z is the inner product of
+% lists X and Y with respect to binary operations + and *. This kind of inner product is also
+% called a dot product. You may assume that lists X and Y have the same length. Example:
+% inner([1,2,3], [4,5,6], 32), because 1*4+2*5+3*6 = 32.
+
+
+% 2. Write this predicate using Prolog: outer(X,Y,Z) succeeds when Z is the outer product of lists X
+% and Y with respect to binary operation *. This kind of outer product is also called a
+% multiplication table. Here Z is a list with same length as X, each of whose elements is a sublist
+% with same length as Y. The kth element of the j
+% th sublist is obtained by multiplying the j
+% th
+% element of X and the kth element of Y. Example: outer([1,2,3], [4,5,6,7],
+% [[4,5,6,7],[8,10,12,14],[12,15,18,21]]).
+
+% 3. Write this predicate using Prolog: scan(L, Q) succeeds if L and Q are lists of numbers, and Q is
+% obtained by summing each possible prefix of L. Example: scan([2,3,5,7,11,13], Q) succeeds
+% with Q = [0,2,5,10,17,28,41].
+
+
+% scan2(L, Q) :- 
+
+% 4. Suppose a Prolog database currently contains only facts of the form parent(x, y), which
+% means that x is a parent of y. Write Prolog rules for each of the following:
+% • child(A, B), which means that A is a child of B.
+% • grandchild(A, B), which means that A is a grandchild of B.
+% • sibling(A, B), which means that A is a brother or sister of B.
+% • cousin(A, B), which means that A is a (first) cousin of B.
+% • ancestor(A, B), which means that A is an ancestor of B.
+% • descendant(A, B), which means that A is a descendant of B.
+
+
+% 5. List five (or more) most significant ways that Prolog is not a pure logic programming
+% language, and/or that Prolog does not provide the full power of logic.
+
+% 6. Write this predicate using Prolog: prime(N) succeeds if N is a prime number. Examples:
+% prime(11) succeeds, but prime(12) fails.
+
+% 7. Write this predicate using Prolog: selection_sort(X, Y) succeeds if Y is the list that is
+% obtained by sorting the elements of list X into ascending order. Your code must use the
+% selection sort algorithm, which at each step locates the smallest remaining value and places
+% it at its correct position in the list. Do not use any other sorting algorithm such as insertion
+% sort, merge sort, or quick sort. Example: selection_sort([9, 5, 2, 7, 4, 0], L) succeeds with L
+% = [0, 2, 4, 5, 7, 9].
+
+% 8. Write this predicate in Prolog: sublistSum (L, N, S) succeeds if S is a sublist of list L and the
+% sum of the values in S is N. Example: sublistSum([7,3,4,6,1], 12, S) succeeds with S =
+% [7,4,1]. Note that L and N are the input values and S is the output value.
+
+% 9. Write this predicate in Prolog: disjoint(X, Y) succeeds if lists X and Y contain none of the
+% same values. Examples: disjoint([5,1,9,3,7], [2,6,0,8,4]) succeeds, but
+% disjoint([5,1,9,3,7], [2,6,3,0,8,4]) fails. Do not assume the existence of any predefined
+% predicates that operate on lists.
+
+% 10. Write this predicate using Prolog: diagonal(M, D) succeeds if M is a matrix stored as a
+% list of row lists, and D is a list of its main diagonal elements. Example:
+% diagonal([[a,b,c],[d,e,f],[g,h,i]], D) succeeds with D = [a,e,i].
+% �
+% a b c
+% d e f
+% g h i
+% �
+
+% 11. Write this predicate using Prolog: eval(E, Z) succeeds if Z is the value of expression E.
+% Here an expression is essentially a binary tree with leaves labeled “num( _ )” and
+% internal nodes labeled “add( _,_ )”, “sub( _,_ )”, “mul( _,_ )”, or “div( _,_ )”. Example:
+% eval(mul(add(num(4),num(7)), sub(num(5),num(2))), Z) succeeds with Z = (4+7)*(5-2) = 33. 
+
+
+% 12. Given this Prolog database:
+% split(P, Q, L) :- append(P, Q, L), alldiff(P, Q).
+% append([ ], Y, Y).
+% append([H|T], Y, [H|Z]) :- append(T, Y, Z).
+% alldiff([ ], [ ]).
+% alldiff([H|T], [X|Y]) :- diff(H, X), alldiff(T, Y).
+% diff(a, b).
+% diff(b, a).
+% Draw the proof tree or search tree that Prolog constructs to satisfy this query:
+% split(M, N, [a,b,b,a,b,a,a,b]). Do not show parts of the search that lead only to dead ends
+% and backtracking, but do show all nodes of the search that contribute to proving the query
+% is satisfied. Also, specify the values of M and N for which this query succeeds.
