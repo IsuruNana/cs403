ac879f863edf7dae6f684df32c584c39e028c139
Isuru Nanayakkara <16419053+IsuruNana@users.noreply.github.com>
2018-12-08 14:59:19 -0600

    Did most of fall2018 Prolog

--- prolog/exams2/fall2018.pl
+++ prolog/exams2/fall2018.pl
@@ -2,19 +2,11 @@
 such that R! ≤ F. Examples: the query invfact(24, R) yields R = 4, and the query invfact(23, R)
 yields R = 3. [20 points]
 
-invfact(F, R) :- getFactorialList(F, 1, FactList), getHighest(FactList, R).
+invfact(F, R) :- helper(F, 1, 1, R).
 
-% getFactorialList(0, [1]).
-getFactorialList(F, F, []).
-getFactorialList(F, I, R) :- 0 is mod(F, I), J is I+1, getFactorialList(F, J, A), append(A, [I], R).
-getFactorialList(F, I, R) :- 1 is mod(F, I), J is I+1, getFactorialList(F, J, R).
-
-% 
-
-invfact(F, R) :- helper(F, 1, R).
-
-helper(1, _, 1)
-helper(F, I, R) :- Y 
+helper(F, F, R, R).
+helper(F, N, I, R) :- J is I+1, Y is N*J, Y =< F, helper(F, Y, J, R).
+helper(F, _, I, R) :- helper(F, F, I, R). 
 
 % 2. even_length(L) succeeds iff L is a list with even length, and odd_length(L) succeeds iff L is a list
 % with odd length. Examples: even_length([a,b,c,d]) and odd_length([a,b,c,d,e]) succeed, but
@@ -32,15 +24,16 @@ odd_length([_,_| T]) :- odd_length(T).
 % Example: the query removeDup([a,b,c,a,d,d,b,e,f,c,a,a,f,e], R) yields R = [a,b,c,d,e,f] or any other
 % permuation of this list. [20 points]
 
-% removeDup([], []).
-% removeDup([H|T], R) :- removeDup(T, X), not(member(H, X)), append(X, [H], R).
-% removeDup([H|T], R) :- member(H, X), removeDup(T, X).
+removeDup([], []).
+removeDup([H|T], [H|X]) :- removeDup(T, X), not(member(H, X)).
+removeDup([_|T], R) :- removeDup(T, R).
 
-removeDup(L, R) :- help(L, [], R).
+% or
 
-help([], L, L).
-help([H|T], A, B) :- not(member(H, A)), append(A, [H], Y), help(T, Y, B).
-help([H|T], A, B) :- help(T, A, B).
+removeDup2([], []).
+removeDup2([H|T], L) :- member(H, L), !, removeDup2(T, L).
+removeDup2([H|T], [H|L]) :- not(member(H, T)), removeDup2(T, L).
+
 
 % 4. levelorder(T, L) takes tree T which is represented using a nested list as in the examples below,
 % and it returns the nodes of the level-order traversal of T in the list L. Examples:
@@ -55,8 +48,12 @@ help([H|T], A, B) :- help(T, A, B).
 %         b       c       d
 %     e    f      g     h    j  
 %         k m         n   p
-            
 
+levelorder(T, L) :- help(T, [ ], L). % middle parameter is a queue
+
+help([ ], [ ], [ ]).
+help([ ], [H|T], R) :- help(H, T, R).
+help([H|T], Q, [H|Z]) :- append(Q, T, X), help([ ], X, Z)
 
 % 5. Peano arithmetic represents whole numbers using the successor function s( ). Therefore s(0)
 % represents 1, s(s(0)) represents 2, s(s(s(0))) represents 3, s(s(s(s(0)))) represents 4, etc. [60 points]
@@ -65,11 +62,15 @@ help([H|T], A, B) :- help(T, A, B).
 % a. convert(P, R) converts Peano number P to whole number R. Example: convert(s(s(s(0))), R)
 % yields R = 3.
 
+convert(0, 0).
+convert(s(X), R) :- convert(X, Y), R is Y+1.
 
 % b. less(X, Y) succeeds iff Peano number X is smaller than Peano number Y. Examples:
 % less(s(s(0)), s(s(s(s(0))))) succeeds, but less(s(s(s(s(0)))), s(s(0))) fails. Do not use any
 % arithmetic operators such as +, -, *, /, div, mod, =, <, >, etc.
 
+less(0, s(Y)).
+less(s(X), s(Y)) :- less(X, Y).
 
 % c. add(X, Y, R) adds the Peano numbers X and Y, and returns the result in Peano number R.
 % Example: add(s(s(0)), s(s(s(0))), R) yields R = s(s(s(s(s(0))))). Do not use any arithmetic
